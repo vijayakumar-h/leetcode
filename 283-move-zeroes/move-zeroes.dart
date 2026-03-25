bd0ff12d892c8292bbafd95e343bd658dad0afa4
Vijayakumar H <118760167+vijayakumar-h@users.noreply.github.com>
2026-03-25 23:19:31 +0530

    Time: 1 ms (100.00%) | Memory: 154.3 MB (90.58%) - LeetSync

--- 283-move-zeroes/move-zeroes.dart
+++ 283-move-zeroes/move-zeroes.dart
@@ -1,12 +1,21 @@
 class Solution {
   void moveZeroes(List<int> nums) {
-    List<int> result = [];
-    for(int i in nums){
-        if(i == 0){
-            result.add(i);
+    int size = nums.length;
+    if(size ==0 || size == 1){
+        return;
+    }
+    int nz = 0;
+    int z = 0;
+    while(nz < size){
+        if(nums[nz] != 0){
+            int temp = nums[nz];
+            nums[nz] = nums[z];
+            nums[z] = temp;
+            nz++;
+            z++;
+        }else{
+            nz++;
         }
     }
-    nums.removeWhere((e) => e == 0);
-    nums.addAll(result);
   }
 }
