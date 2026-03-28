969718bf08ceb8e4ad1e8edce979a72a18fcf709
Vijayakumar H <118760167+vijayakumar-h@users.noreply.github.com>
2026-03-28 14:15:08 +0530

    Time: 0 ms (100.00%) | Memory: 149.2 MB (65.20%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    int slow = 0;
+    for(int fast=1; fast < nums.length; fast++){
+        if(nums[slow] != nums[fast]){
+            slow++;
+            nums[slow] = nums[fast];
+        }
+    }
+    return slow+1;
+  }
+}
