46a841b42f5e0a10bb0904e59946536b0ae1af02
Vijayakumar H <118760167+vijayakumar-h@users.noreply.github.com>
2026-09-15 22:09:51 +0530

    Time: 29 ms (29.05%) | Memory: 146.6 MB (94.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.dart
+++ 242-valid-anagram/valid-anagram.dart
@@ -1,10 +1,21 @@
 class Solution {
   bool isAnagram(String s, String t) {
-    String a = (s.split('')..sort()).join('');
-    String b = (t.split('')..sort()).join('');
-    if(a == b){
-        return true;
+    if(s.length != t.length) return false;
+
+    Map<String, int> charCount = {};
+    
+    for(int i=0; i<s.length; i++){
+        String char = s[i];
+        charCount[char] = (charCount[char]?? 0) +1;
     }
-    return false;
+
+    for(int i=0; i<t.length; i++){
+        String char = t[i];
+        if(!charCount.containsKey(char) || charCount[char] == 0){
+            return false;
+        }
+        charCount[char] = charCount[char]! - 1;
+    }
+    return true;
   }
 }
