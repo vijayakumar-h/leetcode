f628be79fbd60883f513cf7723d002a7c8fa068e
Vijayakumar H <118760167+vijayakumar-h@users.noreply.github.com>
2026-09-15 20:16:06 +0530

    Time: 37 ms (21.79%) | Memory: 153.5 MB (7.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.dart
+++ 242-valid-anagram/valid-anagram.dart
@@ -1,9 +1,7 @@
 class Solution {
   bool isAnagram(String s, String t) {
-    final List<String> s1 = s.split('')..sort();
-    final List<String> t1 = t.split('')..sort();
-    final String a = s1.join();
-    final String b = t1.join();
+    String a = (s.split('')..sort()).join('');
+    String b = (t.split('')..sort()).join('');
     if(a == b){
         return true;
     }
